0588d8892cd79422c0f968feff68fd1d8acf1de8
GriffinAnnshual <griffintbr@gmail.com>
2024-08-30 22:16:54 +0530

    fixes

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,1 +1,1 @@
-export const url = import.meta.env.PROD ? import.meta.env.VITE_BACKEND_URL_PRODUCTION : import.meta.env.VITE_BACKEND_URL_LOCAL
+export const url = !import.meta.env.PROD ? import.meta.env.VITE_BACKEND_URL_PRODUCTION : import.meta.env.VITE_BACKEND_URL_LOCAL
