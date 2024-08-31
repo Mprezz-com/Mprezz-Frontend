87c9f5c4ee98f5dc8aa2685ff5e107746ff86a63
Guhan-K <guhan5936@gmail.com>
2024-08-31 10:35:31 +0530

    changes in backend

--- utils/api_routes.js
+++ utils/api_routes.js
@@ -1,8 +1,8 @@
-export const host = "https://mprezz-backend.onrender.com"
+// export const host = "https://mprezz-backend.onrender.com"
+// export const host = "http://mprezz-backend-production.up.railway.app";
 // export const host = 'http://localhost:8000'
+const host = process.env.REACT_APP_BACKEND_API;
 
-
-
-export const orderProductRoute = `${host}/payments/create_order/`
-export const orderVerifyRoute = `${host}/payments/verify/payment/`
-export const enrollStudentRoute = `${host}/payments/createEnrollment/`
+export const orderProductRoute = `${host}/payments/create_order/`;
+export const orderVerifyRoute = `${host}/payments/verify/payment/`;
+export const enrollStudentRoute = `${host}/payments/createEnrollment/`;
