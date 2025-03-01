f66c543fb3f7a10cc1cdef3d0cbe34791f78f1de
GriffinAnnshual <griffintbr@gmail.com>
2025-03-01 19:10:57 +0530

    update backend URL to new deployment

--- src/assets/constants.js
+++ src/assets/constants.js
@@ -1,3 +1,3 @@
 // export const url = 'https://mprezz-backend.onrender.com/'
-export const url = "https://mprezz-backend-production.up.railway.app/";
+export const url = "https://mprezz-backend-z598.onrender.com/"
 // export const url = 'http://localhost:8000/'
